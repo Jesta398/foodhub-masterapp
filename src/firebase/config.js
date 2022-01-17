bf90c6ddd2e18c08eb21928a4656f10ad6283d02
Jesta398 <84431985+Jesta398@users.noreply.github.com>
2022-01-17 18:35:26 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,20 +1,20 @@
 // firebase config
-export const firebaseConfig =  {
-  apiKey: "AIzaSyBLFleiY0-9DLqQo_BS8UgKhoKT2DJl9TE",
-  authDomain: "food-8f2c7.firebaseapp.com",
-  projectId: "food-8f2c7",
-  storageBucket: "food-8f2c7.appspot.com",
-  messagingSenderId: "737275822320",
-  appId: "1:737275822320:web:263d1dcc5d5416edf6c235",
-  measurementId: "G-N1RC13R94T"
-  };
+// export const firebaseConfig =  {
+//   apiKey: "AIzaSyBLFleiY0-9DLqQo_BS8UgKhoKT2DJl9TE",
+//   authDomain: "food-8f2c7.firebaseapp.com",
+//   projectId: "food-8f2c7",
+//   storageBucket: "food-8f2c7.appspot.com",
+//   messagingSenderId: "737275822320",
+//   appId: "1:737275822320:web:263d1dcc5d5416edf6c235",
+//   measurementId: "G-N1RC13R94T"
+//   };
   
-  // export const firebaseConfig = {
-  //   apiKey: "AIzaSyBPpn7YN6ytpvNB1BSXBCIlbW8D--CnwhE",
-  //   authDomain: "foodhub-e4d05.firebaseapp.com",
-  //   projectId: "foodhub-e4d05",
-  //   storageBucket: "foodhub-e4d05.appspot.com",
-  //   messagingSenderId: "497185462406",
-  //   appId: "1:497185462406:web:aebf75cfb2a53f797d5907",
-  //   measurementId: "G-WB1D1NY989"
-  // };
+  export const firebaseConfig = {
+    apiKey: "AIzaSyBPpn7YN6ytpvNB1BSXBCIlbW8D--CnwhE",
+    authDomain: "foodhub-e4d05.firebaseapp.com",
+    projectId: "foodhub-e4d05",
+    storageBucket: "foodhub-e4d05.appspot.com",
+    messagingSenderId: "497185462406",
+    appId: "1:497185462406:web:aebf75cfb2a53f797d5907",
+    measurementId: "G-WB1D1NY989"
+  };
